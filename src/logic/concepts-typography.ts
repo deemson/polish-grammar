cd8501db73d63b0b3f4f14a8db06d26470f98cf7
Kurkin <deemson.k@gmail.com>
2025-01-07 12:54:56 +0100

    initial

---
+++ src/logic/concepts-typography.ts
@@ -0,0 +1,11 @@
+import type { GrammaticalCase } from './concepts.ts'
+
+export const grammaticalCaseTitles: Record<GrammaticalCase, string> = {
+  nominative: 'Nominative',
+  genitive: 'Genitive',
+  dative: 'Dative',
+  accusative: 'Accusative',
+  instrumental: 'Instrumental',
+  locative: 'Locative',
+  vocative: 'Vocative'
+}
---
+++ src/logic/concepts.ts
@@ -0,0 +1,33 @@
+export const grammaticalCases = [
+  'nominative',
+  'genitive',
+  'dative',
+  'accusative',
+  'instrumental',
+  'locative',
+  'vocative'
+] as const
+
+export type GrammaticalCase = typeof grammaticalCases[number]
+
+export const grammaticalNumbers = [
+  'singular',
+  'plural'
+] as const
+
+export type GrammaticalNumber = typeof grammaticalNumbers[number]
+
+export const grammaticalSingularGenders = [
+  'masculine',
+  'feminine',
+  'neuter'
+] as const
+
+export type GrammaticalSingularGender = typeof grammaticalSingularGenders[number]
+
+export const grammaticalPluralGenders = [
+  'masculine',
+  'non-masculine'
+] as const
+
+export type GrammaticalPluralGender = typeof grammaticalPluralGenders[number]
